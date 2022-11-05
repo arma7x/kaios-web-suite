760e8847b95f1c5e6a2706c0d5c0abd5fd10b532
Ahmad Malik <arma7x@live.com>
2022-11-05 23:22:57 +0800

    WIP:CHAT

--- chrome-extension/src/welcome/routes/Chat.svelte
+++ chrome-extension/src/welcome/routes/Chat.svelte
@@ -8,11 +8,51 @@
 
     export let params = {};
 
+    export interface MessageIndex {
+      index: number,
+      message: SyncProtocol.MozSmsMessage|SyncProtocol.MozMmsMessage,
+    }
+
     let messages: Array<SyncProtocol.MozSmsMessage|SyncProtocol.MozMmsMessage> = [];
+    let messageIndex: {[key: string|number]: MessageIndex;} = {};
 
     function streamEvent(evt) {
-        if (evt.detail.type === SyncProtocol.SMS_GET_MESSAGES && evt.detail.data.threadId === params.threadId) {
-            messages = evt.detail.data.messages;
+        switch (evt.detail.type) {
+            case SyncProtocol.SMS_GET_MESSAGES:
+                if (evt.detail.data.threadId == params.threadId) {
+                    evt.detail.data.messages.forEach((message, index) => {
+                        messageIndex[message.id] = { index, message };
+                    });
+                    messages = evt.detail.data.messages;
+                }
+                break;
+            case SyncProtocol.SMS_ON_DELIVERY_ERROR:
+            case SyncProtocol.SMS_ON_DELIVERY_SUCCESS:
+            case SyncProtocol.SMS_ON_RECEIVED:
+            case SyncProtocol.SMS_ON_RETRIEVING:
+            case SyncProtocol.SMS_ON_SENT:
+            case SyncProtocol.SMS_ON_SENDING:
+            case SyncProtocol.SMS_ON_FAILED:
+                if (evt.detail.data.message.threadId == params.threadId) {
+                    const message = evt.detail.data.message;
+                    if (messageIndex[message.id] == null) {
+                        messages.push(message);
+                        messageIndex[message.id] = { index: messages.length - 1, message };
+                    } else {
+                        messageIndex[message.id].message = message;
+                        messages[messageIndex[message.id].index] = messageIndex[message.id].message;
+                    }
+                }
+                break;
+            case SyncProtocol.SMS_DELETE_MESSAGE:
+                evt.detail.data.request.forEach((messageId, idx) => {
+                    if (messageIndex[messageId] && evt.detail.data.response[idx] == 1) {
+                        const index = messageIndex[messageId].index;
+                        delete messageIndex[messageId];
+                        messages.splice(index, 1);
+                    }
+                });
+                break;
         }
     }
 
@@ -37,7 +77,12 @@
 
 <div>
     <h2>Thread {params.threadId}</h2>
+    <ul>
+        {#each messages as message}
+            <li>{ JSON.stringify(message) }</li>
+        {/each}
+    </ul>
     <p>
-        { JSON.stringify(messages) }
+        { JSON.stringify(messageIndex) }
     </p>
 </div>
