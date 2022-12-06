af0d7f1a7031470d7212d378cdd5f682f856b953
Ahmad Malik <arma7x@live.com>
2022-12-06 15:16:39 +0800

    carddav to mozContact

--- chrome-extension/src/dashboard/routes/contacts/CardDAVContacts.svelte
+++ chrome-extension/src/dashboard/routes/contacts/CardDAVContacts.svelte
@@ -37,6 +37,22 @@
                         const splitURL = contact.url.split('/');
                         temp[splitURL[splitURL.length - 1]] = contact;
                         contact.data = new vCard().parse(contact.data);
+                        const { fn, n, tel } = contact.data.data;
+                        let mozCt = {};
+                        mozCt['name'] = [fn._data];
+                        const name = n._data.split(';');
+                        ['familyName', 'givenName', 'additionalName', 'honorificPrefix', 'honorificSuffix'].forEach((field, index) => {
+                            mozCt[field] = name[index] ? [name[index]] : null;
+                        });
+                        if (tel.length) {
+                            mozCt['tel'] = [];
+                            tel.forEach(t => {
+                                mozCt['tel'].push({ "type": [t.type], "value": t._data });
+                            });
+                        } else {
+                            mozCt['tel'] = [{ "type": [tel.type], "value": tel._data }];
+                        }
+                        // console.log(splitURL[splitURL.length - 1], mozCt);
                     });
                     contactList = {...temp};
                 } catch(err) {
@@ -64,6 +80,6 @@
     <h1>CardDAVContacts</h1>
     <div><button on:click={getContact}>getContact</button></div>
     {#each Object.entries(contactList) as [key, contact]}
-        <div style="margin-bottom:4px;">{key}: { contact.data.data.fn._data }, { contact.data.data.tel._data }</div>
+        <div style="margin-bottom:4px;">{key}: { contact.data.data.fn._data }, { contact.data.data.tel.length ? contact.data.data.tel[0]._data : contact.data.data.tel._data }</div>
     {/each}
 </div>
